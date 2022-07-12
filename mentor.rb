73f860d4b5a42db2f3ee24688fa2522ec9204ab1
kazuhiro.isashiki <k.isashiki@gmail.com>
2022-07-12 11:54:22 +0000

    継承とオーバーライドのコーディング

---
+++ mentor.rb
@@ -0,0 +1,23 @@
+class Mentor
+  attr_accessor :name
+  
+  def initialize(name)
+    self.name=name
+  end
+  
+  def job
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+  end 
+end
+
+class RaisMentor < Mentor
+  def job
+    puts  "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+  end
+end
+
+kirameki=Mentor.new('煌木')
+akaide=RaisMentor.new('赤出')
+
+kirameki.job
+akaide.job
